a72b292fef0ccd7c55ae21c9aca450031cfd49b5
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2024-07-24 00:03:34 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('../home/home.component').then(m => m.HomeComponent),
-    title: 'Home'
+    title: 'Adam Khairi - Web Developer'
   },
   {
     path: '404',
